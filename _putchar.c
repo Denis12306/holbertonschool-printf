a5bb33d96d22550bc79615290a018ddc870766f6
Nano420x <jdjjsudjdjdhs@gmail.com>
2025-11-25 15:51:23 +0100

    add . on comms on putchar file

--- _putchar.c
+++ _putchar.c
@@ -4,7 +4,7 @@
  * The putchar function in C writes a single character to the standard output
  * (usually the console) and returns the character written or EOF on error.
  * It is used for outputting characters one at a time,
- * often in loops or for debugging purposes
+ * often in loops or for debugging purposes.
  */
 
 int _putchar(char c)
