9d46524cfc5cd9bda7c3e805ef38176014e98a74
Denis12306 <denis.gavaud@gmail.com>
2025-11-25 13:26:12 +0000

    main.h

---
+++ main.h
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+/**
+ * struct format-specifier - type of specifier in input
+ * @format : the type of data %, s, c, d, f... 
+ * @function : arguments of va_list
+ * The structure allow us to connect a format to a function
+ */
+typedef struct format_specifier
+{
+	char *format;
+	int (*function)(va_list args);
+}
+	format_specifier;
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_character(va_list args);
+int print_integer(va_list args);
+int print_strings(va_list args);
+int (*function_library(char s))(va_list));
+#endif
